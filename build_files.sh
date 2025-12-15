aefbfba808c8125b9fa7788856a9bd601e2606f3
Eduardoedy17 <eduardoedy1717@gmail.com>
2025-12-15 13:00:23 -0300

    ...

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 echo "Building project..."
+# Instala as dependências
 python3.12 -m pip install -r requirements.txt
 
-echo "Make Migration..."
-python3.12 manage.py makemigrations
-python3.12 manage.py migrate
-
 echo "Collect Static..."
+# Gera os arquivos estáticos (Essencial para o CSS funcionar)
 python3.12 manage.py collectstatic --noinput --clear
+
+# COMENTE AS MIGRAÇÕES NO BUILD (Faça isso via terminal local)
+# echo "Make Migration..."
+# python3.12 manage.py makemigrations
+# python3.12 manage.py migrate
 
 echo "Build End"

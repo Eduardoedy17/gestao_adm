e93fe92e9a5235e41deea794d887801e9ef7c3ae
Eduardoedy17 <eduardoedy1717@gmail.com>
2026-01-07 21:06:56 -0300

    ..

--- build_files.sh
+++ build_files.sh
@@ -3,9 +3,9 @@
 echo "Building project..."
 python3.12 -m pip install -r requirements.txt
 
-# echo "Make Migration..."             <-- COMENTE ESTA LINHA
-# python3.12 manage.py makemigrations  <-- COMENTE ESTA LINHA
-# python3.12 manage.py migrate         <-- COMENTE ESTA LINHA
+echo "Make Migration..."             
+python3.12 manage.py makemigrations  
+python3.12 manage.py migrate         
 
 echo "Collect Static..."
 python3.12 manage.py collectstatic --noinput --clear
